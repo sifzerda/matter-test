354e8972dea4ae65eaaf19c6c4d93e3297d4e4d0
Troy <tydamon@hotmail.com>
2024-08-19 23:45:09 +1000

    css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
 // src/pages/Home.jsx
 
 function Home() {
-    return <h1>Home Page</h1>;
+    return (
+        <div>
+    <h1>Matter.js Demos</h1> 
+<p>Demos I made: <a href="/bubbles">Bubbles</a>, <a href="/mousetrap">Mousetrap</a>, <a href="/ballpit">Ballpit</a>, <a href="/bike">Bike</a>
+</p>
+
+        </div>
+    );
 }
 
 export default Home;
+
+
